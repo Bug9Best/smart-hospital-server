f509fc41e83e6aaa5394db6a7ee2b23eb904d7b9
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-25 04:32:58 +0700

    update topbar

--- frontend/src/app/layout/topbar/topbar.component.ts
+++ frontend/src/app/layout/topbar/topbar.component.ts
@@ -1,14 +1,30 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "../service/layout.service";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './topbar.component.html',
 })
 export class AppTopBarComponent {
+    currentUser = JSON.parse(localStorage.getItem('user')!);
 
     items!: MenuItem[];
+    menuitems: MenuItem[] = [
+        {
+            items: [
+                {
+                    label: 'ออกจากระบบ',
+                    icon: 'pi pi-sign-out',
+                    command: () => {
+                        localStorage.removeItem('user');
+                        this.router.navigate(['/signin']);
+                    }
+                },
+            ]
+        }
+    ];
 
     @ViewChild('menubutton') menuButton!: ElementRef;
 
@@ -16,5 +32,5 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService, private router: Router) { }
 }
