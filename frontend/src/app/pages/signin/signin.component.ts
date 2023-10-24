b1757cff420a357939442f23eea8d95f58662f84
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-25 01:59:44 +0700

    update

--- frontend/src/app/pages/signin/signin.component.ts
+++ frontend/src/app/pages/signin/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { StaffRole } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -8,11 +12,29 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class SigninComponent {
 
+  formData: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
   constructor(
+    private router: Router,
+    private messageService: MessageService,
     private authService: AuthService
   ) { }
 
   signin() {
-    
+    let values = this.formData.value;
+    this.authService.loginStaff(values).subscribe({
+      next: (response: any) => {
+        localStorage.setItem('user', response);
+        if (response.role === StaffRole.STAFF) {
+          this.router.navigate(['/']);
+        }
+      },
+      error: (error: any) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.message });
+      }
+    });
   }
 }
