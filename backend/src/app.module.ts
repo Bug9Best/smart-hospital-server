f0b94d90c3a7e037504012535d7254ee782d029a
pisitchai rueangwatanaphong <pisitchai.rueangwatanaphong@gmail.com>
2023-10-24 23:58:06 +0700

    delete: all and create schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,32 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { AppointModule } from './appoint/appoint.module';
-import { QueueModule } from './queue/queue.module';
-import { RecordModule } from './record/record.module';
-import { ScheduleModule } from './schedule/schedule.module';
-import { NotificationModule } from './notification/notification.module';
-import { DrugModule } from './drug/drug.module';
-import { StaffModule } from './staff/staff.module';
-import { ProfileModule } from './profile/profile.module';
-import { EventService } from './event/event.service';
-import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    EventModule,
-    ProfileModule,
-    StaffModule,
-    DrugModule,
-    NotificationModule,
-    ScheduleModule,
-    RecordModule,
-    QueueModule,
-    AppointModule
-  ],
-  controllers: [AppController],
-  providers: [AppService, EventService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
